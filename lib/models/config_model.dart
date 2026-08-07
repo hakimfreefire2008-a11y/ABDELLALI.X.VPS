09250b2fb6ecd354a870f1d095f59d2528cf7e6f
hakimfreefire2008-a11y <hakimfreefire@gmail.com>
2026-08-07 21:35:08 +0100

    Fix dependencies and config model

--- lib/models/config_model.dart
+++ lib/models/config_model.dart
@@ -1,206 +1,161 @@
-// lib/models/config_model.dart
-
 class ConfigModel {
-  final String id;
+
+
   final String name;
-  final String note;
   final String type;
+  final String note;
+
+
+  final String host;
+  final String port;
+
+
+  final String username;
+  final String password;
+
+
+  final bool useSni;
+  final String sni;
+
+
+  final bool usePayload;
+  final String payload;
+
+
+  final bool useProxy;
+  final String proxyHost;
+  final String proxyPort;
+
+
+  final String v2rayType;
+  final String uuid;
+  final String address;
+
+
+  final String nsDomain;
+  final String publicKey;
+  final String dnsIp;
+
+
   final DateTime createdAt;
-  final DateTime updatedAt;
-  final bool isFavorite;
-  final bool isActive;
-  final String? sshHost;
-  final int? sshPort;
-  final String? sshUsername;
-  final String? sshPassword;
-  final String? httpPayload;
-  final String? httpProxy;
-  final bool enableSni;
-  final String? sniDomain;
-  final String? v2rayAddress;
-  final int? v2rayPort;
-  final String? v2rayUuid;
-  final String? v2rayHost;
-  final String? v2rayPath;
-  final String? v2raySni;
-  final String? v2rayNetwork;
-  final String? v2raySecurity;
-  final String? v2rayType;
-  final String? slowDnsNsDomain;
-  final String? slowDnsPublicKey;
-  final String? slowDnsDnsIp;
-  final String? udpServer;
-  final int? udpPort;
-  final String? filePath;
+
+
 
   ConfigModel({
-    required this.id,
+
     required this.name,
-    this.note = '',
+
     required this.type,
-    DateTime? createdAt,
-    DateTime? updatedAt,
-    this.isFavorite = false,
-    this.isActive = true,
-    this.sshHost,
-    this.sshPort,
-    this.sshUsername,
-    this.sshPassword,
-    this.httpPayload,
-    this.httpProxy,
-    this.enableSni = false,
-    this.sniDomain,
-    this.v2rayAddress,
-    this.v2rayPort,
-    this.v2rayUuid,
-    this.v2rayHost,
-    this.v2rayPath,
-    this.v2raySni,
-    this.v2rayNetwork,
-    this.v2raySecurity,
-    this.v2rayType,
-    this.slowDnsNsDomain,
-    this.slowDnsPublicKey,
-    this.slowDnsDnsIp,
-    this.udpServer,
-    this.udpPort,
-    this.filePath,
-  })  : createdAt = createdAt ?? DateTime.now(),
-        updatedAt = updatedAt ?? DateTime.now();
 
-  Map<String, dynamic> toJson() => {
-        'id': id,
-        'name': name,
-        'note': note,
-        'type': type,
-        'createdAt': createdAt.toIso8601String(),
-        'updatedAt': updatedAt.toIso8601String(),
-        'isFavorite': isFavorite,
-        'isActive': isActive,
-        'sshHost': sshHost,
-        'sshPort': sshPort,
-        'sshUsername': sshUsername,
-        'sshPassword': sshPassword,
-        'httpPayload': httpPayload,
-        'httpProxy': httpProxy,
-        'enableSni': enableSni,
-        'sniDomain': sniDomain,
-        'v2rayAddress': v2rayAddress,
-        'v2rayPort': v2rayPort,
-        'v2rayUuid': v2rayUuid,
-        'v2rayHost': v2rayHost,
-        'v2rayPath': v2rayPath,
-        'v2raySni': v2raySni,
-        'v2rayNetwork': v2rayNetwork,
-        'v2raySecurity': v2raySecurity,
-        'v2rayType': v2rayType,
-        'slowDnsNsDomain': slowDnsNsDomain,
-        'slowDnsPublicKey': slowDnsPublicKey,
-        'slowDnsDnsIp': slowDnsDnsIp,
-        'udpServer': udpServer,
-        'udpPort': udpPort,
-        'filePath': filePath,
-      };
+    required this.note,
 
-  factory ConfigModel.fromJson(Map<String, dynamic> json) => ConfigModel(
-        id: json['id'],
-        name: json['name'],
-        note: json['note'] ?? '',
-        type: json['type'],
-        createdAt: DateTime.parse(json['createdAt']),
-        updatedAt: DateTime.parse(json['updatedAt']),
-        isFavorite: json['isFavorite'] ?? false,
-        isActive: json['isActive'] ?? true,
-        sshHost: json['sshHost'],
-        sshPort: json['sshPort'],
-        sshUsername: json['sshUsername'],
-        sshPassword: json['sshPassword'],
-        httpPayload: json['httpPayload'],
-        httpProxy: json['httpProxy'],
-        enableSni: json['enableSni'] ?? false,
-        sniDomain: json['sniDomain'],
-        v2rayAddress: json['v2rayAddress'],
-        v2rayPort: json['v2rayPort'],
-        v2rayUuid: json['v2rayUuid'],
-        v2rayHost: json['v2rayHost'],
-        v2rayPath: json['v2rayPath'],
-        v2raySni: json['v2raySni'],
-        v2rayNetwork: json['v2rayNetwork'],
-        v2raySecurity: json['v2raySecurity'],
-        v2rayType: json['v2rayType'],
-        slowDnsNsDomain: json['slowDnsNsDomain'],
-        slowDnsPublicKey: json['slowDnsPublicKey'],
-        slowDnsDnsIp: json['slowDnsDnsIp'],
-        udpServer: json['udpServer'],
-        udpPort: json['udpPort'],
-        filePath: json['filePath'],
-      );
 
-  ConfigModel copyWith({
-    String? id,
-    String? name,
-    String? note,
-    String? type,
-    DateTime? createdAt,
-    DateTime? updatedAt,
-    bool? isFavorite,
-    bool? isActive,
-    String? sshHost,
-    int? sshPort,
-    String? sshUsername,
-    String? sshPassword,
-    String? httpPayload,
-    String? httpProxy,
-    bool? enableSni,
-    String? sniDomain,
-    String? v2rayAddress,
-    int? v2rayPort,
-    String? v2rayUuid,
-    String? v2rayHost,
-    String? v2rayPath,
-    String? v2raySni,
-    String? v2rayNetwork,
-    String? v2raySecurity,
-    String? v2rayType,
-    String? slowDnsNsDomain,
-    String? slowDnsPublicKey,
-    String? slowDnsDnsIp,
-    String? udpServer,
-    int? udpPort,
-    String? filePath,
-  }) {
-    return ConfigModel(
-      id: id ?? this.id,
-      name: name ?? this.name,
-      note: note ?? this.note,
-      type: type ?? this.type,
-      createdAt: createdAt ?? this.createdAt,
-      updatedAt: updatedAt ?? this.updatedAt,
-      isFavorite: isFavorite ?? this.isFavorite,
-      isActive: isActive ?? this.isActive,
-      sshHost: sshHost ?? this.sshHost,
-      sshPort: sshPort ?? this.sshPort,
-      sshUsername: sshUsername ?? this.sshUsername,
-      sshPassword: sshPassword ?? this.sshPassword,
-      httpPayload: httpPayload ?? this.httpPayload,
-      httpProxy: httpProxy ?? this.httpProxy,
-      enableSni: enableSni ?? this.enableSni,
-      sniDomain: sniDomain ?? this.sniDomain,
-      v2rayAddress: v2rayAddress ?? this.v2rayAddress,
-      v2rayPort: v2rayPort ?? this.v2rayPort,
-      v2rayUuid: v2rayUuid ?? this.v2rayUuid,
-      v2rayHost: v2rayHost ?? this.v2rayHost,
-      v2rayPath: v2rayPath ?? this.v2rayPath,
-      v2raySni: v2raySni ?? this.v2raySni,
-      v2rayNetwork: v2rayNetwork ?? this.v2rayNetwork,
-      v2raySecurity: v2raySecurity ?? this.v2raySecurity,
-      v2rayType: v2rayType ?? this.v2rayType,
-      slowDnsNsDomain: slowDnsNsDomain ?? this.slowDnsNsDomain,
-      slowDnsPublicKey: slowDnsPublicKey ?? this.slowDnsPublicKey,
-      slowDnsDnsIp: slowDnsDnsIp ?? this.slowDnsDnsIp,
-      udpServer: udpServer ?? this.udpServer,
-      udpPort: udpPort ?? this.udpPort,
-      filePath: filePath ?? this.filePath,
-    );
+    required this.host,
+
+    required this.port,
+
+
+    required this.username,
+
+    required this.password,
+
+
+    required this.useSni,
+
+    required this.sni,
+
+
+    required this.usePayload,
+
+    required this.payload,
+
+
+    required this.useProxy,
+
+    required this.proxyHost,
+
+    required this.proxyPort,
+
+
+    required this.v2rayType,
+
+    required this.uuid,
+
+    required this.address,
+
+
+    required this.nsDomain,
+
+    required this.publicKey,
+
+    required this.dnsIp,
+
+
+    required this.createdAt,
+
+  });
+
+
+
+  Map<String,dynamic> toJson(){
+
+    return {
+
+      "name":name,
+
+      "type":type,
+
+      "note":note,
+
+
+      "host":host,
+
+      "port":port,
+
+
+      "username":username,
+
+      "password":password,
+
+
+      "useSni":useSni,
+
+      "sni":sni,
+
+
+      "usePayload":usePayload,
+
+      "payload":payload,
+
+
+      "useProxy":useProxy,
+
+      "proxyHost":proxyHost,
+
+      "proxyPort":proxyPort,
+
+
+      "v2rayType":v2rayType,
+
+      "uuid":uuid,
+
+      "address":address,
+
+
+      "nsDomain":nsDomain,
+
+      "publicKey":publicKey,
+
+      "dnsIp":dnsIp,
+
+
+      "createdAt":
+      createdAt.toIso8601String(),
+
+
+    };
+
   }
+
+
 }
