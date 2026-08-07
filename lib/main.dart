b0ec4ece8e13f003b0f2710acbebddc634562813
hakimfreefire2008-a11y <hakimfreefire@gmail.com>
2026-08-07 03:55:58 +0100

    Load online servers on app startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,65 @@
 import 'package:flutter/material.dart';
+
+import 'services/server_service.dart';
 import 'screens/home_screen.dart';
 
-void main() {
-  runApp(const ConfigesApp());
+
+
+Future<void> main() async {
+
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+
+  await ServerService.loadServers();
+
+
+  runApp(
+    const ConfigesApp(),
+  );
+
+
 }
 
+
+
 class ConfigesApp extends StatelessWidget {
+
+
   const ConfigesApp({super.key});
+
+
 
   @override
   Widget build(BuildContext context) {
+
+
     return MaterialApp(
+
       debugShowCheckedModeBanner: false,
 
-      title: 'CONFIGES ABDELLALI PRO',
+
+      title: "CONFIGES ABDELLALI PRO",
+
 
       theme: ThemeData(
-        useMaterial3: true,
+
         brightness: Brightness.dark,
+
         colorSchemeSeed: Colors.blue,
+
+        useMaterial3: true,
+
       ),
 
+
       home: const HomeScreen(),
+
+
     );
+
+
   }
+
+
 }
