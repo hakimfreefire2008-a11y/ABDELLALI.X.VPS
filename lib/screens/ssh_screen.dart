275376dd227769dca66efbb62ba12cb2cf42ff4f
hakimfreefire2008-a11y <hakimfreefire@gmail.com>
2026-08-07 03:17:21 +0100

    Add copy share buttons to SSH servers

--- lib/screens/ssh_screen.dart
+++ lib/screens/ssh_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../services/server_service.dart';
+import '../services/action_service.dart';
 
 
 class SshScreen extends StatelessWidget {
@@ -32,41 +33,96 @@ class SshScreen extends StatelessWidget {
 
         itemBuilder: (context,index){
 
-
           final server = servers[index];
+
+
+          final config =
+              "Host: ${server.host}\n"
+              "Type: ${server.type}\n"
+              "Country: ${server.country}";
 
 
           return Card(
 
-            child: ListTile(
+            child: Padding(
 
-              leading: Text(
-                server.country,
-                style: const TextStyle(
-                  fontSize: 25,
-                ),
-              ),
+              padding: const EdgeInsets.all(12),
 
+              child: Column(
 
-              title: Text(
-                server.name,
-              ),
+                crossAxisAlignment: CrossAxisAlignment.start,
+
+                children: [
 
 
-              subtitle: Text(
-                "${server.host}\nPing: ${server.ping} ms",
-              ),
+                  Text(
+                    "${server.country} ${server.name}",
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
 
 
-              trailing: Icon(
+                  const SizedBox(height: 8),
 
-                server.online
-                    ? Icons.check_circle
-                    : Icons.cancel,
 
-                color: server.online
-                    ? Colors.green
-                    : Colors.red,
+                  Text(
+                    server.host,
+                  ),
+
+
+                  Text(
+                    "Ping: ${server.ping} ms",
+                  ),
+
+
+                  const SizedBox(height: 10),
+
+
+                  Row(
+
+                    children: [
+
+
+                      FilledButton.icon(
+
+                        onPressed: (){
+
+                          ActionService.copyConfig(config);
+
+                        },
+
+                        icon: const Icon(Icons.copy),
+
+                        label: const Text("COPY"),
+
+                      ),
+
+
+                      const SizedBox(width: 10),
+
+
+                      OutlinedButton.icon(
+
+                        onPressed: (){
+
+                          ActionService.shareConfig(config);
+
+                        },
+
+                        icon: const Icon(Icons.share),
+
+                        label: const Text("SHARE"),
+
+                      ),
+
+
+                    ],
+
+                  )
+
+                ],
 
               ),
 
